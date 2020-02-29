7d7bcbd4c22d8741f535f0a14b26c19143f395e8
d.urosov <d.urosov@topof.ru>
2020-02-29 10:24:57 +0300

    верстка главной

--- public/site/script.js
+++ public/site/script.js
@@ -1,7 +1,5 @@
 $(document).ready(e => {
 
-    console.log('load');
-
     $('.index-slider').bxSlider({
         minSlides: 3,
         maxSlides: 3,
@@ -14,4 +12,5 @@ $(document).ready(e => {
         prevText: '.',
         slideWidth: 600
     });
+
 });
