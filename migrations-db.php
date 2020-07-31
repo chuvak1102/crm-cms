2276c39babb48377b39f48af0cec6942d314b083
chuvak1102 <dan0@mail.ru>
2020-07-31 21:12:36 +0300

    + migrations

---
+++ migrations-db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'dbname' => 'u0742521_ecopacking',
+    'user' => 'u0742521_default',
+    'password' => 'j3x3HI!Q',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+];
